895892094917fb726c5928788198098959ca50e2
Daniel <jmzwcn@gmail.com>
2019-01-20 12:42:25 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -124,28 +124,32 @@ export class HomePage implements OnInit {
   }
 
   scanQR() {
-    //this.qrScanner.prepare()
-    // .then((status: QRScannerStatus) => {
-    //   if (status.authorized) {
-    // camera permission was granted
-    this.qrScanner.show();
-    // start scanning
-    let scanSub = this.qrScanner.scan().subscribe((text: string) => {
-      //console.log('Scanned something', text);
-      alert(text);
-      this.qrScanner.hide(); // hide camera preview
-      scanSub.unsubscribe(); // stop scanning
-    });
+    this.qrScanner.prepare()
+      .then((status: QRScannerStatus) => {
+        if (status.authorized) {
+          // camera permission was granted
+          window.document.querySelector('ion-app').classList.add('transparentBody');
+          // start scanning
+          let scanSub = this.qrScanner.scan().subscribe((text: string) => {
+            console.log('Scanned something', text);
+            alert(text);
+            this.qrScanner.hide(); // hide camera preview
+            scanSub.unsubscribe(); // stop scanning
+            window.document.querySelector('ion-app').classList.remove('transparentBody')
+          });
 
-    //    } else if (status.denied) {
-    // camera permission was permanently denied
-    // you must use QRScanner.openSettings() method to guide the user to the settings page
-    // then they can grant the permission from there
-    //  } else {
-    // permission was denied, but not permanently. You can ask for permission again at a later time.
-    //   }
-    // })
-    // .catch((e: any) => console.log('Error is', e));
+        } else if (status.denied) {
+          // camera permission was permanently denied
+          // you must use QRScanner.openSettings() method to guide the user to the settings page
+          // then they can grant the permission from there
+        } else {
+          // permission was denied, but not permanently. You can ask for permission again at a later time.
+        }
+      })
+      .catch((e: any) => {
+        console.log('Error is', e);
+        window.document.querySelector('ion-app').classList.remove('transparentBody')
+      });
   }
 
   async beginNow() {
