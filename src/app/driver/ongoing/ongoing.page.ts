05bf7be0572ae25078988e4d3283739f72aa77ff
Daniel <jmzwcn@gmail.com>
2019-01-26 18:25:57 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/driver/ongoing/ongoing.page.ts
+++ src/app/driver/ongoing/ongoing.page.ts
@@ -35,21 +35,23 @@ export class OngoingPage implements OnInit {
           for (var i in response.getItemsList()) {
             console.log(i, response.getItemsList()[i])
             let tsOrder = response.getItemsList()[i]
-            this.orders[i] = tsOrder.toObject();
-            if (tsOrder.getSender() != null) {
-              this.orders[i].sender = tsOrder.getSender().toObject();
+            if (tsOrder.getStatus() == 'accept') {
+              this.orders[i] = tsOrder.toObject();
+              if (tsOrder.getSender() != null) {
+                this.orders[i].sender = tsOrder.getSender().toObject();
+              }
+              if (tsOrder.getFrom() != null) {
+                this.orders[i].from = tsOrder.getFrom().toObject();
+              }
+              if (tsOrder.getTosList()[0] != null) {
+                this.orders[i].to = tsOrder.getTosList()[0].toObject();
+              }
+              this.orders[i].fee = tsOrder.getFee().toFixed(2);
+              this.orders[i].created = tsOrder.getCreated().toDate();
             }
-            if (tsOrder.getFrom() != null) {
-              this.orders[i].from = tsOrder.getFrom().toObject();
-            }
-            if (tsOrder.getTosList()[0] != null) {
-              this.orders[i].to = tsOrder.getTosList()[0].toObject();
-            }
-            this.orders[i].fee = tsOrder.getFee().toFixed(2);
-            this.orders[i].created = tsOrder.getCreated().toDate();
           };
         }
-        this.orders = this.orders.filter(order => order.status == 'accept');
+        //this.orders = this.orders.filter(order => order.status == 'accept');
       });
   }
 
