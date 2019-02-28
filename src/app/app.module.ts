a2f65e38f80a6530441d6fd9667fe69a5b9c0f4c
Daniel <jmzwcn@gmail.com>
2019-03-01 00:03:58 +0800

    build.
    Signed-off-by: Daniel <jmzwcn@gmail.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+import { HttpClientModule } from '@angular/common/http';
 import { IonicModule, IonicRouteStrategy, IonSlides } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
@@ -22,7 +22,8 @@ import { OrderComponent } from './modal/order/order.component';
     BrowserModule,
     IonicModule.forRoot(),
     AppRoutingModule,
-    FormsModule
+    FormsModule,
+    HttpClientModule
   ],
   providers: [
     StatusBar,
--- src/app/settings/settings.page.ts
+++ src/app/settings/settings.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-settings',
@@ -6,10 +8,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./settings.page.scss'],
 })
 export class SettingsPage implements OnInit {
+  privacy: any;
 
-  constructor() { }
+  constructor(
+    public alertController: AlertController,
+    public http: HttpClient) { }
 
   ngOnInit() {
+    this.http.get('https://raw.githubusercontent.com/freightio/client/master/src/assets/privacy.txt').subscribe(
+      response => {
+        console.log(response);
+      }, err => {
+        console.log(err.error.text);
+        this.privacy = err.error.text;
+      });
   }
 
+  async popupPrivacy() {
+    const alert = await this.alertController.create({
+      header: '隐私声明',
+      message: this.privacy,
+      buttons: ['OK']
+    });
+
+    await alert.present();
+  }
 }
